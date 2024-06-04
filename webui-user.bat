d0d0a82e48c405990a67e8d04e2b16f53f61403b
lattecatte <154484150+lattecatte@users.noreply.github.com>
2024-06-04 11:24:20 +0900

    Add arguments for directml

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --autolaunch --use-directml --precision full --no-half --no-half-vae --opt-split-attention-v1 --opt-sub-quad-attention --disable-nan-check
 
 call webui.bat
